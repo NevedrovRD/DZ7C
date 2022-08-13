4a289fef4049fee146d4fa217304543f302ffe2f
Roman <ramec154@mail.ru>
2022-08-13 13:10:42 +0300

    DZ7 исправление

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -24,5 +24,5 @@ for (int i = 0; i < m; i++)
 }
 
 for (int j = 0; j < n; j++)
-    Console.Write("" + string.Join(", " , array[j] / 3) + ";  ");
+    Console.Write("" + string.Join(", " , array[j] / m) + ";  ");
 
